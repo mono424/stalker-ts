ca7886ef6b2c094df643aa2437c9fc07451a7456
Khadim Fall <khadimfall0602@gmail.com>
2025-09-23 23:02:36 +0200

    add csv and table

--- src/storage.ts
+++ src/storage.ts
@@ -52,3 +52,104 @@ export function influxdb2Storage(
     },
   };
 }
+
+function sessionsToRows(sessions: StalkerSession[]): {
+  header: string[];
+  rows: (string | number | null)[][];
+} {
+  const header = [
+    "time",
+    "duration",
+    "sessionName",
+    "eventName",
+    "eventStartTime",
+    "eventDuration",
+  ];
+
+  const rows = sessions
+    .reduce((acc, session) => {
+      const eventPoints = session.events.map((event) => {
+        let eventRow = [
+          event.time,
+          event.duration,
+          session.name,
+          event.name,
+          event.duration,
+          ...Array(header.length - 5).fill(null),
+        ];
+
+        if (event.payload) {
+          for (const [key, value] of Object.entries(event.payload)) {
+            const columnIndex = header.indexOf(key);
+            if (columnIndex === -1) {
+              header.push(key);
+              eventRow.push(value);
+            } else {
+              eventRow[columnIndex] = value;
+            }
+          }
+        }
+        return eventRow;
+      });
+      return [...acc, ...eventPoints];
+    }, [] as string[][])
+    .map((row) => [
+      ...(row ? row : []),
+      ...Array(header.length - row.length).fill(null),
+    ]);
+  return { header, rows };
+}
+
+export function tableStorage(): Storage {
+  return {
+    saveSessions: async (sessions: StalkerSession[]) => {
+      const { header, rows } = sessionsToRows(sessions);
+      console.table(
+        rows.map((row) =>
+          Object.fromEntries(header.map((col, i) => [col, row[i]])),
+        ),
+      );
+    },
+  };
+}
+
+export type CsvOptions = {
+  sep: string;
+  header: boolean;
+  newline: string;
+  quote: string;
+  callback?: (csv: string) => void;
+};
+
+export const defaultCsvOptions: CsvOptions = {
+  sep: ",",
+  header: true,
+  newline: "\n",
+  quote: '"',
+};
+
+export function csvStorage(options: Partial<CsvOptions>): Storage {
+  const csvOptions = { ...defaultCsvOptions, ...options };
+  return {
+    saveSessions: async (sessions: StalkerSession[]) => {
+      const { header, rows } = sessionsToRows(sessions);
+      const csv = rows.map((row) =>
+        row
+          .map((cell) => `${csvOptions.quote}${cell}${csvOptions.quote}`)
+          .join(options.sep),
+      );
+      if (csvOptions.header) {
+        csv.unshift(
+          header
+            .map((col) => `${csvOptions.quote}${col}${csvOptions.quote}`)
+            .join(csvOptions.sep),
+        );
+      }
+      if (csvOptions.callback) {
+        csvOptions.callback(csv.join(csvOptions.newline));
+      } else {
+        console.log(csv.join(csvOptions.newline));
+      }
+    },
+  };
+}
